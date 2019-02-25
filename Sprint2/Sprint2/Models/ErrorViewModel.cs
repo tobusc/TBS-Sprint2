c9f43ce059dfde57251bfa55ae256ef4c7a353cc
Tomas Bustamante Schwember <tschwember0262@conestogac.on.ca>
2019-02-24 22:20:26 -0500

    Initial Commit

---
+++ Sprint2/Sprint2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sprint2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
